a9abbaf6a951b33e97258925cb5c96333f96507f
UlisesCeca <ulises@ulisesceca.com>
2018-08-11 19:22:32 +0200

    d

--- src/main/java/com/ulises/errorsloggerserver/ErrorLoggerServerApplication.java
+++ src/main/java/com/ulises/errorsloggerserver/ErrorLoggerServerApplication.java
@@ -8,13 +8,15 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ErrorLoggerServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ErrorLoggerServerApplication.class, args);
+		if(loginAppServer())
+			SpringApplication.run(ErrorLoggerServerApplication.class, args);
 	}
 
 
@@ -31,5 +33,9 @@ public class ErrorLoggerServerApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	private static boolean loginAppServer() {
+		return true;
 	}
 }
