b520e8c46cdd64dfca5eb3e8009db455a98d3da4
UlisesCeca <ulises@ulisesceca.com>
2018-08-12 13:03:08 +0200

    -

--- src/main/java/com/ulises/errorsloggerserver/ErrorLoggerServerApplication.java
+++ src/main/java/com/ulises/errorsloggerserver/ErrorLoggerServerApplication.java
@@ -6,16 +6,23 @@ import com.ulises.errorsloggerserver.rest.exceptionsmappers.NoErrorsFoundsForApp
 import com.ulises.errorsloggerserver.rest.exceptionsmappers.NotFoundMapper;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 @SpringBootApplication
 public class ErrorLoggerServerApplication {
 
 	public static void main(String[] args) {
-		if(loginAppServer())
+		if(loginAppServer(args[0], args[1]))
 			SpringApplication.run(ErrorLoggerServerApplication.class, args);
 	}
 
@@ -35,7 +42,20 @@ public class ErrorLoggerServerApplication {
 		return new ModelMapper();
 	}
 
-	private static boolean loginAppServer() {
-		return true;
+
+	private static boolean loginAppServer(final String user, final String password) {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		URL url;
+		HttpURLConnection con;
+		try {
+			url = new URL("http://localhost:8680/ulises/apperver/app/getAll");
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Authorization", "a");
+			con.getInputStream();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
--- src/main/java/com/ulises/errorsloggerserver/constants/Constants.java
+++ src/main/java/com/ulises/errorsloggerserver/constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String RESPONSE_ERROR_NO_ERRORS_FOUND = "No Errors found for this App.";
     public static final String REQUEST_ERROR_NULL_BODY = "Request's body must not be null.";
     public static final String REQUEST_ERROR_INTERNAL_SERVER_ERROR = "There has been a server error and your request " +
-            "couldn't be processed.";
+            "couldn't be processed. Please, try later";
     public static final String REQUEST_ERROR_NOT_FOUND = "This endpoint doesn't exist.";
     public static final String RESPONSE_OK_ERROR_SAVED = "Error saved successfully.";
 }
